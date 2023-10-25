806c5479dc471a10c65a93de691fbf44fb6e89bd
pujadas <pd8652944@gmail.com>
2023-10-25 23:05:34 +0530

    Second Version

---
+++ secondBoot/src/main/java/org/anudip/secondBoot/bean/EmpBean.java
@@ -0,0 +1,28 @@
+package org.anudip.secondBoot.bean;
+
+public class EmpBean {
+	int id;
+	String name;
+	public EmpBean() {
+		super();
+	}
+	public EmpBean(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
